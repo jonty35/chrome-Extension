767427c4f34d9b7ebe534fcf2a5de3078211555a
Jonty Meshram <jontymeshramk@gmail.com>
2025-06-16 23:31:50 +0530

    no message

--- background.js
+++ background.js
@@ -1,18 +1,29 @@
 
-
-
 function MyGenericClick(info, tab) {
   console.log("clicked on page :", info, tab);
 }
 
+function MyImageClick(info,tab)
+{
+  console.log("clicked on image :",info , tab);
+}
 
 chrome.runtime.onInstalled.addListener(() =>{
 chrome.contextMenus.create({
-    id:"my-checkbox",
+    id:"my-share",
     title: "share",
     contexts:["page"],
 
 });
+chrome.contextMenus.create({
+    id:"my-image",
+    title: "share image",
+    contexts:["image"],
+
+});
 });
 
+
+
 chrome.contextMenus.onClicked.addListener(MyGenericClick);
+chrome.contextMenus.onClicked.addListener(MyImageClick);
