df8fcf57f038cdd0dedfc060bbcca5dc8678c6b3
Jonty Meshram <jontymeshramk@gmail.com>
2025-06-18 17:41:19 +0530

    no message

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ console.log("My Extension");
 
 var links = document.getElementsByTagName("a");
 
-for(let i = 0; i < links.length; i++ )
-{
-    let title =links[i].innerHTML;
+for (let i = 0; i < links.length; i++) {
+    let title = links[i].innerHTML;
     console.log("Link is ", title);
 }
